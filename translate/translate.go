5746b1ce3a314b665a134df4f03256d5a2c676fe
pstadler1990 <pstadler90@gmx.de>
2019-05-14 14:43:31 +0200

    Translate works

--- translate/translate.go
+++ translate/translate.go
@@ -2,6 +2,7 @@ package translate
 
 import (
 	"bufio"
+	"errors"
 	"os"
 	"regexp"
 )
@@ -25,16 +26,40 @@ func readJsonRaw(path string) ([]string, error) {
 	return lines, scanner.Err()
 }
 
-func Translate(keywordsPath, translatedPath string) {
-	translatedSliceRaw, errTranslated := readJsonRaw(translatedPath)
-	//keywordsSlice, errKeywords := readJsonRaw(keywordsPath)
+func writeTranslatedSliceToFile(slice []string, path string) error {
+	file, err := os.Create(path)
 
-	if errTranslated != nil /*|| errKeywords != nil*/ {
-		//...
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	for _, s := range slice {
+		_, err := file.WriteString(s)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func Translate(keywordsPath, translatedPath string) error {
+	translatedSliceRaw, errTranslated := readJsonRaw(translatedPath)
+	keywordsSliceRaw, errKeywords := readJsonRaw(keywordsPath)
+
+	if errTranslated != nil {
+		return errTranslated
+	}
+
+	if errKeywords != nil {
+		return errKeywords
 	}
 
 	/* Selector for extracted_keywords file */
-	//reKeywords := regexp.MustCompile(`"(.+)": \d+`)
+	reKeywords := regexp.MustCompile(`"(.+)": \d+`)
+	reKeywordsValue := regexp.MustCompile(`"(.+)"`)
 
 	/* Selector for bow_translated file */
 	reBow := regexp.MustCompile(`\s*"(.+)",?\s*`)
@@ -43,9 +68,26 @@ func Translate(keywordsPath, translatedPath string) {
 
 	for _, s := range translatedSliceRaw {
 		if reBow.MatchString(s) {
-			translatedSlice = append(translatedSlice, reBow.ReplaceAllString(s, "$1"))
+			translatedSlice = append(translatedSlice, reBow.ReplaceAllString(s, "\"$1\""))
 		}
 	}
+
+	if translatedSlice == nil {
+		return errors.New("Translated slice is nil")
+	}
+
+	var keywordsSlice []string
+
+	counter := 0
+	for _, s := range keywordsSliceRaw {
+		if reKeywords.MatchString(s) {
+			/* Overwrite file string */
+			s = reKeywordsValue.ReplaceAllString(s, translatedSlice[counter])
+			counter += 1
+		}
+		/* Write raw string (both modified and original) */
+		keywordsSlice = append(keywordsSlice, s)
+	}
+
+	return writeTranslatedSliceToFile(keywordsSlice, "out.json")
 }
-
-
