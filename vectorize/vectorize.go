6130acc09f14ba50e383553e01405bba254336c3
pstadler1990 <pstadler90@gmx.de>
2019-05-16 22:16:22 +0200

    removed unused struct type

--- vectorize/vectorize.go
+++ vectorize/vectorize.go
@@ -1,11 +1,6 @@
 package vectorize
 
-type Bow struct {
-	word  string
-	count int
-}
-
-func CreateVector(fPath string) {
+func CreateVectors(bowArticle, bowSubpages map[string]interface{}) {
 	/* Reads in two bag of words of type Bow,
 	   and creates two vectors of these files.
 	   Missing words are added to the vector with count 0 */
