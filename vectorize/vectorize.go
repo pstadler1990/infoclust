5c476965f7e87938022ce3436b0b70524123f5fe
pstadler1990 <pstadler90@gmx.de>
2019-05-20 22:16:15 +0200

    removed vectorize package as we directly deal with slices / maps

--- vectorize/vectorize.go
+++
@@ -1,8 +0,0 @@
-package vectorize
-
-func CreateVectors(bowArticle, bowSubpages map[string]interface{}) {
-	/* Reads in two bag of words of type Bow,
-	   and creates two vectors of these files.
-	   Missing words are added to the vector with count 0 */
-
-}
