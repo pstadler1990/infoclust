9fbd94e96db9553cd702f6e0854ef87629b52e78
pstadler1990 <pstadler90@gmx.de>
2019-05-21 18:31:10 +0200

    added cosine distance function

--- cosine/cosine.go
+++ cosine/cosine.go
@@ -1,8 +1,11 @@
 package cosine
 
-import _ "github.com/atedja/go-vector"
+import "github.com/atedja/go-vector"
 
-func Distance() float64 {
-
-	return 0.0
+func Distance(vectorA, vectorB vector.Vector) (error, float64) {
+	dot, err := vector.Dot(vectorA, vectorB)
+	if err != nil {
+		return err, 0.0
+	}
+	return nil, float64(dot / (vectorA.Magnitude() * vectorB.Magnitude()))
 }
